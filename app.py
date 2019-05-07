446e4f13a121a5c13c8540e9a5d188c01d35b6f0
Matt <mwcooley99@gmail.com>
2019-05-06 17:34:20 -0700

    added scrape function

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
+import random
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    l = list('ABCDEF')
+    choice = random.choice(l)
+    return render_template('index.html', choice=choice)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ scrape_mars.py
@@ -0,0 +1,96 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+
+import requests
+import subprocess
+
+import time
+
+from splinter import Browser
+
+driver = subprocess.run(['which', 'chromedriver'],
+                        stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+executable_path = {'executable_path': driver}
+
+
+def scraper(url):
+    with Browser('chrome', **executable_path, headless=True) as browser:
+        browser.visit(url)
+        time.sleep(.5)
+        html = browser.html
+    return html
+
+
+def scrape():
+    # Create dict to hold data
+    scrape_dict = {}
+
+    # 1. Nasa News Website
+    url1 = 'https://mars.nasa.gov/news/'
+    html1 = scraper(url1)
+
+    # create the BS object
+    bs1 = BeautifulSoup(html1, 'html.parser')
+
+    # Get the first title and it's description
+    scrape_dict['news_title'] = bs1.find('div',
+                                         {'class': 'content_title'}).text
+    scrape_dict['news_description'] = bs1.find('div', {
+        'class': 'article_teaser_body'}).text
+
+    # 2. Image Search
+    base_url = 'https://www.jpl.nasa.gov'
+    url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    html = scraper(url2)
+    bs2 = BeautifulSoup(html, 'html.parser')
+
+    image_route = bs2.find('a', {'class': 'button fancybox'})[
+        'data-fancybox-href']
+    scrape_dict['featured_image'] = base_url + image_route
+
+    # 3. mars weather
+    url3 = 'https://twitter.com/marswxreport?lang=en'
+    html3 = scraper(url3)
+    bs3 = BeautifulSoup(html3, 'html.parser')
+
+    scrape_dict['mars_weather'] = bs3.find('div', {
+        'class': 'js-tweet-text-container'}).text.strip()
+
+    # 4. Mars Facts
+    url4 = 'https://space-facts.com/mars/'
+    html4 = scraper(url4)
+    bs4 = BeautifulSoup(html4, 'lxml')
+
+    mars_facts = str(bs4.find('table', {'id': 'tablepress-mars'}))
+
+    scrape_dict['mars_facts_df'] = pd.read_html(mars_facts)
+
+    # 5. Mars Hemispheres
+    base_url = 'https://astrogeology.usgs.gov'
+    res = requests.get(
+        'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars')
+    soup = BeautifulSoup(res.content, 'lxml')
+
+    # Get links to the hemisphere pages
+    items = soup.find_all('div', {'class': 'item'})
+    links = [base_url + item.a['href'] for item in items]
+
+    # open each of the websites and store the conent
+    hemisphere_image_urls = []
+    for link in links:
+        temp_dict = {}
+        res = requests.get(link)
+        soup = BeautifulSoup(res.content, 'lxml')
+        temp_dict['url'] = soup.find('a', {'target': '_blank'})['href']
+        temp_dict['name'] = soup.find('h2', {'class': 'title'}).text.replace(
+            ' Enhanced', '')
+        hemisphere_image_urls.append(temp_dict)
+
+    scrape_dict['hemisphere_image_urls'] = hemisphere_image_urls
+
+    return scrape_dict
+
+
+if __name__ == '__main__':
+    for k, v in scrape().items():
+        print(v)
